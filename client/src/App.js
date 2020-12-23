93a54995f40641b9682882fb4722aedd313a9927
arun-ajay <arunajay42795@gmail.com>
2020-12-23 00:48:06 -0500

    landing page bio + transition and photo

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,Route,
+  Switch,
+  Redirect
+} from "react-router-dom"
 
+import Landing from "./pages/Landing/landing"
+
+import {Container} from 'semantic-ui-react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className = "app">
+      <Router>
+        <Switch>
+          <Route exact path = "/">
+            <Landing></Landing>
+          </Route>
+          <Redirect from = "/*" to = "/"/>
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
---
+++ client/src/pages/Landing/landing.jsx
@@ -0,0 +1,74 @@
+import React,{Component} from 'react';
+import './landing.css';
+
+import UdayabhanuPhoto from "./assets/Udayabhanu.jpg"
+
+import {Grid,Card,Transition,Image,Container} from 'semantic-ui-react'
+
+export default class Landing extends Component{
+
+    state = {
+
+        englishDescription : "I am a Seeker of truthful History; so, I look for that in the wider spectrum  and more"+ 
+        "in the unscripted annals of history, not just in the "+ 
+        "scripted history written by special interest groups.",
+         malayalamDescription : "ഞാൻ സത്യമായ ചരിത്രം അന്വേഷിക്കുന്ന ചരിത്രാന്വേഷി. അതിനായി, ചിലർ സ്വന്തം കാര്യസാദ്ധ്യത്തിനായി,"+ 
+        " ചരിത്രം നിർമ്മിക്കാൻ വേണ്ടി എഴുതിയ ചരിത്രത്തേക്കാൾ കൂടുതലായി, ചരിത്രവ്യക്തിത്വങ്ങളുടെ സ്വകാര്യ ലിഖിതങ്ങളിൽ തേടുന്നു.",
+        open: false
+    }
+
+
+    componentDidMount(){
+        this.setState({open: true})
+    }
+    render () { 
+        
+        return(
+            <Grid stackable style = {{"border":"5px solid red"}} padded = {"vertically"} >
+                <Grid.Row>
+                    HEADER STUFF HERE
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column verticalAlign = {"middle"} width ={4}>
+                <Transition
+                        animation = "fade right"
+                        duration = {1000}
+                        visible = {this.state.open}
+                        >
+                            <Card centered >
+                                <Image src= {UdayabhanuPhoto} wrapped ui={false} />
+                            </Card>
+                    </Transition>
+                    </Grid.Column>
+
+                    <Grid.Column width = {12}>
+                    <Transition
+                        animation = "fade right"
+                        duration = {1000}
+                        visible = {this.state.open}
+                        >
+                        <Container fluid>
+                            <Container fluid textAlign = 'justified'>
+                                <p className = "englishQuote"> 
+                                    {this.state.englishDescription}
+                                        <br></br>
+                                        <br></br>
+                                    {this.state.malayalamDescription}
+                                </p>
+                            </Container>
+                            <Container fluid textAlign = "right">
+                                <p className = "author">
+                                <br></br>
+                                - by Udayabhanu Panickar
+
+                                </p>
+                            </Container>
+
+                        </Container>
+                    </Transition>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        )
+    } 
+}
