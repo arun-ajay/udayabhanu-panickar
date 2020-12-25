39324025be2fd46330f1c8f9a916ea980518fe41
arun-ajay <arunajay42795@gmail.com>
2020-12-25 02:28:29 -0500

    header styling

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -25,22 +25,33 @@ export default class SiteHeader extends Component{
         return(
             <Grid.Row>
                 <Grid.Column width = {16}>
-                    <Menu stackable className = {styles.customMenu}>
+                    <Menu borderless stackable className = {styles.customMenu}>
                         <Menu.Item header>Udayabhanu Panickar</Menu.Item>
+                        <Menu.Item
+                        name='home'
+                        active={activeItem === 'home'}
+                        onClick={this.handleItemClick}
+                        className = {this.state.activeItem == "home" ? styles.customMenuSelected : styles.customMenu}
+                        >
+                            <Icon name = 'home'/>
+                        </Menu.Item>
                         <Menu.Item
                         name='About Me'
                         active={activeItem === 'About Me'}
                         onClick={this.handleItemClick}
+                        className = {this.state.activeItem == "About Me" ? styles.customMenuSelected  : styles.customMenu}
                         />
                         <Menu.Item
                         name='Spiritual Writings'
                         active={activeItem === 'Spiritual Writings'}
                         onClick={this.handleItemClick}
+                        className = {this.state.activeItem == "Spiritual Writings" ? styles.customMenuSelected  : styles.customMenu}
                         />
                         <Menu.Item
                         name='Historical Writings'
                         active={activeItem === 'Historical Writings'}
                         onClick={this.handleItemClick}
+                        className = {this.state.activeItem == "Historical Writings" ? styles.customMenuSelected  : styles.customMenu}
                         />
                         <Menu.Menu stackable position='right'>
                             <Menu.Item
@@ -52,16 +63,16 @@ export default class SiteHeader extends Component{
 
                                 }}
                                 trigger={
-                                    <Button icon labelPosition='right'>
+                                    <Button icon labelPosition='right' color = 'blue'>
                                       Sign in
                                       <Icon name='sign in alternate' />
                                     </Button>
                                 }
                             >
-                                <Card className = "">
+                                <Card className = {styles.login}>
                                     <Image src= {LoginPhoto} wrapped ui={false} />
                                     <Card.Content textAlign = {"center"}>
-                                        <Card.Header>Sign in with</Card.Header>
+                                        <Card.Header>Sign In With</Card.Header>
                                     </Card.Content>
                                         <Card.Content extra textAlign = {"center"}>
                                             <Button circular color='facebook' icon='facebook' />
